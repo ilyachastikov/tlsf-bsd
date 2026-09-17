c3bc06532f1a5bc0ca9d0bb998d8d1baca6a1a26
Elias Tchastikov <chastland@mail.ru>
2026-09-18 02:34:28 +0300

    fix TLSF_NATIVE_ACALLOC macro in tlsf.h

--- include/tlsf.h
+++ include/tlsf.h
@@ -207,7 +207,7 @@ extern "C" {
 /* Macros for native architecture alignment alloctions */
 #define TLSF_NATIVE_AALLOC(t, size) \
     (tlsf_aalloc((t), TLSF_ARCH_ALIGNMENT, (size)))
-#define TLSF_NATIVE_ACALLOC(t, nmemb, align, size) \
+#define TLSF_NATIVE_ACALLOC(t, nmemb, size) \
     (tlsf_acalloc((t), (nmemb), TLSF_ARCH_ALIGNMENT, (size)))
 #define TLSF_NATIVE_AREALLOC(t, mem, size) \
     (tlsf_arealloc((t), (mem), TLSF_ARCH_ALIGNMENT, (size)))
