f48f1eba5916dc7a3285c244f2ff4cb7084c0511
Elias Tchastikov <chastland@mail.ru>
2026-09-18 02:31:41 +0300

    add validate the alignment before calling the core allocator in tlsf_thread.c

--- src/tlsf_thread.c
+++ src/tlsf_thread.c
@@ -293,6 +293,10 @@ void *tlsf_thread_arealloc(tlsf_thread_t *ts,
     size_t old_size;
     TLSF_LOCK_ACQUIRE(&ts->arenas[idx].lock);
     old_size = tlsf_usable_size(ptr);
+    if (!align || (align & (align - 1))) {
+        TLSF_LOCK_RELEASE(&ts->arenas[idx].lock);
+        return NULL;
+    }
     void *new_ptr = tlsf_arealloc(&ts->arenas[idx].pool, ptr, align, size);
     TLSF_LOCK_RELEASE(&ts->arenas[idx].lock);
 
